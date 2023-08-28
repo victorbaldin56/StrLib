81c108188ba9e2daacce45db6f7f494a3d894fff
victor <victor@MiWiFi-R2350-srv.lan>
2023-08-28 20:15:31 +0300

    initial commit

---
+++ strlib.cpp
@@ -0,0 +1,153 @@
+#include <assert.h>
+#include "strlib.h"
+
+static void swap(char *a, char *b);
+
+// puts fgets strdup getline
+
+char *str_copy(char dest[], const char src[]) {
+    assert(dest);
+    assert(src);
+    assert(dest != src);
+    
+    size_t i = 0;
+    
+    for (i = 0; src[i]; i++) {
+        dest[i] = src[i];
+    }
+
+    dest[i] = '\0';
+    
+    return dest;
+}
+
+size_t str_len(const char str[]) {
+    assert(str);
+
+    size_t i = 0;
+    
+    for (i = 0; str[i]; i++) {
+        ;
+    }
+    
+    return i + 1;
+}
+
+char *str_rev(char str[]) {
+    assert(str);
+
+    size_t len = str_len(str);
+    
+    for (size_t i = 0; i < len; i++) {
+        swap(&str[i], &str[len - 1 - i]);     
+    }
+
+    return str; 
+}
+
+const char *str_chr(const char str[], const char ch) {
+    assert(str);
+
+    size_t i = 0;
+    
+    for (i = 0; str[i]; i++) {
+        if (str[i] == ch) {
+            return str + i;
+        }
+    }
+    
+    if (!ch) {
+        return str + i;
+    }
+
+    return NULL;
+}
+
+int str_cmp(const char l[], const char r[]) {
+    assert(l);
+    assert(r);
+    
+    size_t i = 0;
+    
+    while (l[i] && r[i]) {
+        if (l[i] > r[i]) {
+            return 1;
+        }
+        else if (l[i] < r[i]) {
+            return -1;
+        }
+        i++;
+    }
+    
+    return 0;
+}
+
+char *str_n_cpy(char dest[], const char src[], const size_t count) {
+    assert(dest);
+    assert(src);
+    assert(dest != src);
+
+    size_t i = 0;
+    
+    while (i < count && src[i]) {
+        dest[i] = src[i];
+        i++;
+    }
+
+    while (i < count) {
+        dest[i] = '\0';
+    }
+
+    return dest;
+}
+
+char *str_cat(char dest[], const char src[]) {
+    assert(dest);
+    assert(src);
+    
+    size_t i = 0;
+    
+    for (i = 0; dest[i]; i++) {
+        ;
+    }
+
+    size_t j = 0;
+    
+    while (src[j]) {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+
+    return dest;
+}
+
+char *str_n_cat(char dest[], const char src[], const size_t count) {
+    assert(dest);
+    assert(src);
+
+    size_t i = 0;
+    for (i = 0; dest[i]; i++) {
+        ;
+    }
+    size_t j = 0;
+    
+    while (src[j] && j < count) {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+
+    return dest;
+}
+
+static void swap(char *a, char *b) {
+    char tmp = *b;
+    *b = *a;
+    *a = tmp;
+}
+
---
+++ strlib.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * 
+*/
+char *str_copy(char dest[], const char src[]);
+
+/**
+ * 
+*/
+size_t str_len(const char str[]);
+
+/**
+ * 
+*/
+char *str_rev(char str[]);
+
+/**
+ * 
+*/
+const char *str_chr(const char str[], const char ch);
+
+/**
+ * 
+*/
+int str_cmp(const char l[], const char r[]);
+
+/**
+ * 
+*/
+char *str_n_cpy(char dest[], const char src[], const size_t count);
+
+/**
+ * 
+*/
+char *str_cat(char dest[], const char src[]);
+
+/**
+ * 
+*/
+char *str_n_cat(char dest[], const char src[], const size_t count);
